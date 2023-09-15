244e1b50e4f54802bdd5348d4e1580eaa43a479d
jamesparsons529 <james.parsons2@griffithuni.edu.au>
2023-09-15 13:31:24 +1000

    completed task 1

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect(r'./climate.db')
+cursor = connection.cursor()
+
+cursor.execute("SELECT Year FROM ClimateData")
+yearsResult = cursor.fetchall()
+for i in yearsResult:
+    years.append(i)
+
+cursor.execute("SELECT CO2 FROM ClimateData")
+co2Result = cursor.fetchall()
+for i in co2Result:
+    co2.append(i)
+
+cursor.execute("SELECT Temperature FROM ClimateData")
+tempResult = cursor.fetchall()
+for i in tempResult:
+    temp.append(i)
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
